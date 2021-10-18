9ffe798d96dc08787ba7f8173a1198efe9b6f1bd
mu10-ar <mudassar.hussain0544@gmail.com>
2021-10-18 11:46:05 +0500

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -10,9 +10,9 @@ class Home extends BaseController
     public function index()
     {
         $islogin=false;
-        if ($islogin!='true') {
-             return redirect()->to(base_url().'/login');
-        }
+        // if ($islogin!='true') {
+        //      return redirect()->to(base_url().'/login');
+        // }
         
           echo view('partials/sidebar');
          echo view('doctor/dashboard');
