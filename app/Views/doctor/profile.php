6344ed6a445cb2af0d6ad6155df2681054fe68d4
asohaib865 <64709498+mu10-ar@users.noreply.github.com>
2021-10-18 10:29:05 +0500

    Create profile.php
    Doctors Profile view  Page  Added.
    
    Co-Authored-By: asohaib865 <92565082+asohaib865@users.noreply.github.com>

---
+++ app/Views/doctor/profile.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Bootstrap CSS CDN -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/dropdown.min.css">
+
+    <!-- Font Awesome JS -->
+     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+      <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+                <!-- Doctor Information Page -->
+                <div class="col-sm-12" id="PrintMe">
+        <div class="panel panel-default thumbnail">
+
+
+                        <div class="panel-body">
+                <div class="row">
+
+                    <div class="col-sm-12" align="center">  
+                        <h1>Doctor Information</h1>
+                    <br>
+                                         </div>
+
+                    <div class="col-md-3 col-lg-3 " align="center"> 
+                        <img alt="Picture" src="cv4.jpg" class="img-thumbnail img-responsive">
+                        <h3>
+                            Ali Asghar
+                        </h3>
+                    </div>
+
+                    <div class="col-md-7 col-lg-7 "> 
+                        <dl class="dl-horizontal">
+                            <dt>Email Address</dt><dd>ali.123@gmail.com</dd>
+                            <dt>Designation</dt><dd>Snr. Manager</dd>
+                            <dt>Department</dt><dd>Neurology</dd>
+                            <dt>Address</dt><dd>Ahmedabad Lahore Pakistan</dd>
+                            <dt>Phone No</dt><dd>922223146</dd>
+                            <dt>Mobile No</dt><dd>922223146</dd>
+                            <dt>Specialist</dt><dd></dd>
+                            <dt>Date of Birth</dt><dd>1992-10-10</dd>
+                            <dt>Sex</dt><dd>Male</dd>
+                            <dt>Create Date</dt><dd>2021-10-18</dd>
+                            <dt>Update Date</dt><dd></dd>
+                            <dt>Status</dt><dd>Active</dd>
+                        </dl> 
+                    </div>
+                </div>
+            </div> 
+
+            <div class="panel-footer">
+                <div class="text-center">
+                    <strong>Arkham Asylum</strong>
+                    <p class="text-center">Located in Gotham City, U.S. state of New Jersey </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+   
+  </body>
+</html>
