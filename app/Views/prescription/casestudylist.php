c9a9e4ac07b27991557fa370a7962680d907c247
asohaib865 <64709498+mu10-ar@users.noreply.github.com>
2021-10-20 16:19:12 +0500

    casestudy
    added table for listing castestudies
    deleted dummy files {created for testing purpose}
    
    Co-Authored-By: asohaib865 <92565082+asohaib865@users.noreply.github.com>

--- app/Views/prescription/casestudylist.php
+++ app/Views/prescription/casestudylist.php
@@ -1,4 +1,49 @@
 <!-- CaseStudy  View -->
 <div style="text-align: center; margin:  4px;">
-<h1>CaseStudy</h1>
+<h1>CaseStudy  List</h1>
+</div>
+</div>
+<div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Patient Id</th>
+                        <th scope="col">Food Allergies</th>
+                        <th scope="col">Tendency Bleed</th>
+                        <th scope="col">Heart Disease</th>
+                        <th scope="col">High Blood Pressure</th>
+                        <th scope="col">Diabetic</th>
+                        <th scope="col">Family  Medical History</th>
+                        <th scope="col">Current Medication</th>
+                        <th scope="col">Female Pragnancy</th>
+                        <th scope="col">Breast Feeding</th>
+                        <th scope="col">Health INsurance</th>
+                        <th scope="col">Low Inncome</th>
+                        <th scope="col">Reference</th>
+                        <th scope="col">Action</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>PO5QR4L6</td>
+                        <td>y</td>
+                        <td>y</td>
+                        <td>y</td>
+                        <td>y</td>
+                        <td>y</td>
+                        <td>y</td>
+                        <td>y</td>
+                        <td>y</td>
+                        <td>y</td>
+                        <td>np</td>
+                        <td>Too low</td>
+                        <td>Dean of Hospital</td>
+                        <td>Edit/DElete/View</td>
+                    </tr>
+                   
+                </tbody>
+            </table>
 </div>
--- app/Views/prescription/prescriptionlist.php
+++ app/Views/prescription/prescriptionlist.php
@@ -2,3 +2,32 @@
 <div style="text-align: center; margin:  4px;">
 <h1>PrescriptionS Here!</h1>
 </div>
+<div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Patient Id</th>
+                        <th scope="col">Appointment ID</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Doctor Name</th>
+                        <th scope="col">Visiting Fees</th>
+                        <th scope="col"> Date</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>PQ0R1</td>
+                        <td>asdf</td>
+                        <td>New</td>
+                        <td>M.ALi</td>
+                        <td>1100</td>
+                        <td>26/10/2021 </td>
+                        <td>Edit/DElete/View</td>
+                    </tr>
+                   
+                </tbody>
+            </table>
+</div>
