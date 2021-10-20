3a1da6e0bbd93b9993f64023123e7234d01f5fd0
asohaib865 <64709498+mu10-ar@users.noreply.github.com>
2021-10-20 13:23:28 +0500

    Appointment Done
    Addedcomplete appointment frontend
    
    Co-Authored-By: asohaib865 <92565082+asohaib865@users.noreply.github.com>

---
+++ app/Views/Appointment/viewapointment.php
@@ -0,0 +1,30 @@
+ 
+         <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Patient_id</th>
+                        <th scope="col">Department Name</th>
+                        <th scope="col">Doctor Name</th>
+                        <th scope="col">Appointment Date</th>
+                        <th scope="col">Serial_no</th>
+                        <th scope="col">Problems</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>PQ0R1</td>
+                        <td>Cardiology</td>
+                        <td>Mudassar Hussain</td>
+                        <td>26/10/2021</td>
+                        <td>84</td>
+                        <td>Pain and fatugue </td>
+                        <td>Edit/DElete/View</td>
+                    </tr>
+                   
+                </tbody>
+            </table>
+</div>
