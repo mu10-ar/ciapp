bb1a7e4f0f77db5631fdee73c87cbab1447ac461
mu10-ar <mudassar.hussain0544@gmail.com>
2021-10-21 16:29:04 +0500

    Update servicelist.php

--- app/Views/Billing/servicelist.php
+++ app/Views/Billing/servicelist.php
@@ -1,29 +1,29 @@
 <h1 style="text-align: center; margin:  4px; color:brown">Service List!</h1>
 
 <div class="container">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Service Name</th>
-                        <th scope="col">Description</th>
-                        <th scope="col">Quantity</th>
-                        <th scope="col">Rate</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>root canal treatment</td>
-                        <td>treatment regularly</td>
-                        <td>1</td>
-                        <td>5000</td>
-                        <td>Active</td>
-                        <td>Edit/DElete</td>
-                    </tr>
-                   
-                </tbody>
-            </table>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Service Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Rate</th>
+                <th scope="col">Status</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>root canal treatment</td>
+                <td>treatment regularly</td>
+                <td>1</td>
+                <td>5000</td>
+                <td>Active</td>
+                <td>Edit/DElete</td>
+            </tr>
+
+        </tbody>
+    </table>
 </div>
