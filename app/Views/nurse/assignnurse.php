ef39759e3c57f71f7b517972ac0c4560493e7baf
asohaib865 <64709498+mu10-ar@users.noreply.github.com>
2021-10-23 10:09:00 +0500

    Prescription Redesigned
    Created New
    Prescription Form
    Prescription List
    Assign Nurse Form edited
    
    Co-Authored-By: asohaib865 <92565082+asohaib865@users.noreply.github.com>

---
+++ app/Views/nurse/assignnurse.php
@@ -0,0 +1,28 @@
+<div class="container">
+            <h1 style="text-align: center;">Nurse Assign!</h1>
+
+            <form>
+                <div class="form-group m-3">
+                    <label for=nursename">Nurse Name</label>
+                    <input type="text" class="form-control" id="nursename"
+                        placeholder="Enter Nurse Name">
+                    <small id="nursename" class="form-text"></small>
+                </div>
+                <div class="form-group m-3">
+                    <label for="pateintname">Patient Name</label>
+                    <input type="text" class="form-control" id="pateintname"
+                        placeholder="Enter Patient Name">
+                </div>
+                <div class="form-group m-3">
+                    <label for="departmentname">Department Name</label>
+                    <input type="text" class="form-control" id="departmentname"
+                        placeholder="Enter Department">
+                </div>
+                <div class="form-group form-check m-3">
+                    <input type="checkbox" class="form-check-input" id="check1">
+                    <label class="form-check-label" for="check">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary m-3">Submit</button>
+            </form>
+        </div>
+    
--- app/Views/prescription/addprescription.php
+++ app/Views/prescription/addprescription.php
@@ -3,7 +3,7 @@
     <div style="text-align: center; margin: 4px;">
         <h1>Add Prescription!</h1>
     </div>
-    <form>
+    <form id="form_id">
         <div class="form-row">
             <div class="col-md-4">
                 <div class="form-group mt-3 mb-3">
@@ -33,9 +33,6 @@
                     <span class="red">
                 </div>
 
-
-
-
                 <div class="form-group mb-3">
                     <!-- <label for="appointment_id">Appointment Id</label> -->
                     <input type="text" class="form-control" id="appointment_id" name="appointment_id" placeholder="Appointment Id">
@@ -44,54 +41,37 @@
                     <!-- <label for="date">Date</label> -->
                     <input type="date" class="form-control" id="date" name="date" placeholder="Date">
                 </div>
-                <div class="form-group mb-3">
-                    <!-- <label for="hospital_name">Hospital Name</label> -->
-                    <input type="text" class="form-control" id="hospital_name" name="hopspital_name" placeholder="Hospital
-                                    Name">
-                </div>
+
                 <div class="form-group mb-3">
                     <!-- <label for="address">Address</label> -->
                     <input type="text" class="form-control" id="address" name="address" placeholder="Address">
                 </div>
             </div>
-            <div class="form-group col-md-6">
-                <table class="table table-striped mt-3">
-                    <thead>
-                        <tr class="bg-primary">
-                            <th width="160">Medicine Name</th>
-                            <th width="160">Medicine Type</th>
-                            <th>Instruction</th>
-                            <th width="80">Days</th>
-
-                        </tr>
-                    </thead>
-                    <tbody id="medicine">
-                        <tr>
-                            <td><input type="text" name="medicine_name[]" autocomplete="off" class="medicine form-control" placeholder="Medicine Name"></td>
-                            <td><input type="text" name="medicine_type[]" autocomplete="off" class="form-control" placeholder="Medicine Type"></td>
-                            <td><textarea name="medicine_instruction[]" class="form-control" placeholder="Instruction"></textarea></td>
-                            <td><input type="text" name="medicine_days[]" autocomplete="off" class="form-control" placeholder="Days"></td>
-
-
-                        </tr>
-                    </tbody>
-                </table>
-                <table class="table table-striped">
-                    <thead>
-                        <tr class="bg-danger">
-                            <th width="230">Diagnosis</th>
-                            <th>Instruction</th>
-
-                        </tr>
-                    </thead>
-                    <tbody id="diagnosis">
-                        <tr>
-                            <td><input type="text" name="diagnosis_name[]" autocomplete="off" class="form-control" placeholder="Diagnosis"></td>
-                            <td><textarea name="diagnosis_instruction[]" class="form-control" placeholder="Instruction"></textarea></td>
-                        </tr>
-                    </tbody>
-                </table>
+            <hr>
+            <h3>Medicine</h3>
+            <div class="forn-group col-md-4 mb3">
+                <label for="medicine">Add Medicine </label>
+                <input type="text" class="form-control" name="medicine" placeholder="Add medicine here!">
             </div>
+            <div class="forn-group col-md-4 mb3">
+                <label for="medicine_c">Medicine Category </label>
+                <input type="text" class="form-control" name="medicine_c" placeholder="medicine  category here!">
+            </div>
+            <div class="forn-group col-md-4 mb3">
+                <label for="medicine_d">Medicine Days</label>
+                <input type="text" class="form-control" name="medicine_d" placeholder="how many days?">
+            </div>
+            <hr>
+            <h3>Diagnosis</h3>
+            <div class="forn-group col-md-4 mb3">
+                <label for="diagnosis">Diagnosis</label>
+                <input type="text" class="form-control" name="diagnosis" placeholder="diagnosis here!">
+            </div>
+            <div class="forn-group col-md-4 mb3">
+                <label for="diagnosis_instruc">Diagnosis Instruction</label>
+                <input type="text" class="form-control" name="diagnosis_instruc" placeholder="Diagnosis Instructions here!">
+            </div>
+            <hr>
 
             <div class="form-group col-md-4 mb-3 mt-3">
                 <label for="fees">Visiting Fees</label>
