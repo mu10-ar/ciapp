ac18f90970a0c80a3299c365d9655ba8fd95c5df
mu10-ar <mudassar.hussain0544@gmail.com>
2021-10-23 13:31:22 +0500

    .

--- app/Views/doctor/doctorslist.php
+++ app/Views/doctor/doctorslist.php
@@ -1,46 +1,45 @@
 <!-- Doctors List -->
 <h1 style="text-align: center; margin:  4px;"> Doctors List</h1>
-<div class="container">
-    <table id="myTable" class="table table-striped" style="overflow-x:auto;">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">firstname</th>
-                <th scope="col">lastname</th>
-                <th scope="col">email</th>
-                <th scope="col">address</th>
-                <th scope="col">action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
+
+<table id="myTable" class="table table- table-striped">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">firstname</th>
+            <th scope="col">lastname</th>
+            <th scope="col">email</th>
+            <th scope="col">address</th>
+            <th scope="col">action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
             $session=session();
             $userrole=$session->get('user_role');
             
             $id=1;
             foreach($users as $user){?>
-            <tr>
+        <tr>
 
-                <th scope="row"><?=$id?></th>
-                <td><?=$user['firstname']?></td>
-                <td><?=$user['lastname']?></td>
-                <td><?=$user['email']?></td>
-                <td><?=$user['address']?></td>
+            <th scope="row"><?=$id?></th>
+            <td><?=$user['firstname']?></td>
+            <td><?=$user['lastname']?></td>
+            <td><?=$user['email']?></td>
+            <td><?=$user['address']?></td>
 
 
-                <td>
-                    <?php if (!(($userrole==4)||($userrole==5)||($userrole==6)||($userrole==7)||($userrole==8))):
+            <td>
+                <?php if (!(($userrole==4)||($userrole==5)||($userrole==6)||($userrole==7)||($userrole==8))):
               ?> <a href="<?=base_url()?>/update/<?=$user['id']?>" class="btn btn-primary btn-sm">Edit</a>
-                    <a href="<?=base_url()?>/delete/<?=$user['id']?>" class="btn btn-danger btn-sm">Delete</a>
-                    <?php
+                <a href="<?=base_url()?>/delete/<?=$user['id']?>" class="btn btn-danger btn-sm">Delete</a>
+                <?php
             endif; ?>
-                    <a href="<?=base_url()?>/profile/<?=$user['id']?>" class="btn btn-warning btn-sm">profile</a>
-                </td>
-            </tr>
-            <?php
+                <a href="<?=base_url()?>/profile/<?=$user['id']?>" class="btn btn-warning btn-sm">profile</a>
+            </td>
+        </tr>
+        <?php
              $id++;}?>
 
 
-        </tbody>
-    </table>
-</div>
+    </tbody>
+</table>
--- app/Views/partials/sidebar.php
+++ app/Views/partials/sidebar.php
@@ -6,28 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Admin</title>
+    <!-- <title>Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
 
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
         integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <!-- Our Custom CSS -->
     <link rel="stylesheet" href="<?php echo base_url();?>/assets/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/dropdown.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/dropdown.min.css"> -->
     <!-- Font Awesome JS -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
+    <!-- <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
         integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous">
     </script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
         integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous">
-    </script>
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <script src="/cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
-    <link rel="stylesheet" href="cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+    </script> -->
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script> -->
+    <!-- <script src="/cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script> -->
+    <!-- <link rel="stylesheet" href="cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css"> -->
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <style>
