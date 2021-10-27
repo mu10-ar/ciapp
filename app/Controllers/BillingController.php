ee449be5d4b810c5fb3991c2a3693d0ed0352778
mu10-ar <mudassar.hussain0544@gmail.com>
2021-10-27 10:25:23 +0500

    .

--- app/Controllers/BillingController.php
+++ app/Controllers/BillingController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\models\AppointmentModel;
 use App\models\AssignBedmodel;
 use App\models\bedModel;
+use App\models\BillingModel;
 use App\models\DepartmentModel;
 use App\models\NotificationModel;
 use App\models\UserModel;
@@ -14,11 +15,53 @@ use CodeIgniter\Database\Query;
 
 class BillingController extends BaseController
 {
-    public function addbill(){
+    public function addbill(){ $data=[];
 
 
-        echo "add bill";
-        echo view("partials/sidebar");
+
+        
+        if( $this->request->getMethod()=='post'){
+            $input=$this->validate([
+             'patient_id'=> 'required',
+             'price'=> 'required'
+            ]);
+            
+
+            
+        
+     
+          
+        if ($input==true) {
+            
+                 $bill= new BillingModel();
+                 $bill->save([
+                      'status'=> $this->request->getPost('status'),
+                      'paid'=> $this->request->getPost('paid'),
+                      'price'=> $this->request->getPost('price'),
+                      'services'=> $this->request->getPost('services'),
+                      'services_description'=> $this->request->getPost('services_description'),
+                      'patient_id'=> $this->request->getPost('patient_id'),
+                      'date'=> $this->request->getPost('date'),
+                 ]);
+
+                 return redirect()->to(base_url().'/billlist');
+                                
+        }
+        else {
+                      $data['validation']=$this->validator;
+                 }
+     }
+
+
+
+
+
+
+
+
+        $patient= new UserModel();
+        $data['patient']=$patient->getpatientRecord();
+        echo view("partials/sidebar",$data);
         echo view("billing/addbill");
         echo view("partials/footer");
     }
--- app/Views/Billing/addbill.php
+++ app/Views/Billing/addbill.php
@@ -1,25 +1,47 @@
 <div class="container-fluid">
            <div class="container">
             <div class="container">
-            <h1 style="text-align: center;">Billl!!</h1>
-            <form action="" method="">
+            <h1 style="text-align: center;">Bill!!</h1>
+            <form action="" method="post">
 
-                <div class="form-group">
-                    <label for="pateint_id">Pateint ID</label>
-                    <select type="text" name="patient_id" class="form-control"
-                        id="pateint_id">
-                        <option selected="">Choose...</option>
-                        <option value="">Patient1</option>
-                        <option value="">Patient2</option>
-                        <option value="">Patient3</option>
-                        <option value="">Patient4</option>
-                        </select>
+            <div class="mb-3">
+        <?php
+            $session=session();?>
+                <label for="patientname">Patient Name</label>
+                <select id="patientname" name="patient_id" class="form-control">
+                    
+                    <span class="red">
+                        <?php 
+                                if (isset($validation)&& $validation->hasError('patient_id')) {
 
-                    </div>
+                                    echo $validation->getError('patient_id');
+                                }?>
+                    </span>
+                    <?php
+                        
+                        foreach ($patient as $patient) {?>
+
+
+
+                    <option value="<?=$patient['id']?>"><?=$patient['firstname'].' '.$patient['lastname']?>
+                    </option>
+                    <?php ;} 
+                    ?>
+
+
+                </select>
+        </div>
                     <div class="form-group">
                         <label for="date">Date</label>
                         <input type="date" class="form-control" id="date"
-                            placeholder="Date">
+                            placeholder="Date" name="date">
+                            <span class="red">
+                        <?php 
+                                if (isset($validation)&& $validation->hasError('date')) {
+
+                                    echo $validation->getError('date');
+                                }?>
+                    </span>
                     </div>
                 </div>
                     <div class="container" id="">
@@ -29,26 +51,61 @@
                             <div class="form-group ml-4">
                                 <label for="services">Services</label>
                                 <input type="text" class="form-control"
-                                    name="serv" id="services"
+                                    name="services" id="services"
                                     placeholder="Services">
+                                    <span class="red">
+                        <?php 
+                                if (isset($validation)&& $validation->hasError('services')) {
+
+                                    echo $validation->getError('services');
+                                }?>
+                    </span>
                             </div>
 
 
                             <div class="form-group ml-1">
                                 <label for="description">Description</label>
                                 <input type="text" class="form-control"
-                                    name="descriptoin" id="description"
+                                    name="services_description" id="description"
                                     placeholder="Service Description">
+                                    <span class="red">
+                        <?php 
+                                if (isset($validation)&& $validation->hasError('services_description')) {
+
+                                    echo $validation->getError('services_description');
+                                }?>
+                    </span>
                             </div>
 
                          
                             <div class="form-group ml-1">
                                 <label for="price">Price</label>
-                                <input type="text" class="form-control"
+                                <input type="number" class="form-control"
                                     name="price" id="price"
                                     placeholder="Price">
+                                    <span class="red">
+                        <?php 
+                                if (isset($validation)&& $validation->hasError('price')) {
+
+                                    echo $validation->getError('price');
+                                }?>
+                    </span>
+                            </div>
+                            <div class="form-group ml-1">
+                                <label for="price">paid</label>
+                                <input type="number" class="form-control"
+                                    name="paid" id="price"
+                                    placeholder="Price">
+                                    <span class="red">
+                        <?php 
+                                if (isset($validation)&& $validation->hasError('paid')) {
+
+                                    echo $validation->getError('paid');
+                                }?>
+                    </span>
                             </div>
                         </div>
+                        <input type="hidden" name="status" value="0">
                         </div>
                         <button type="submit" name="button" class="btn btn-primary p-2  ">Add Bill</button>
                     </div>
