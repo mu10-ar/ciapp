713e6939b4fc3d5cc6fda5117a48a102cce6688e
asohaib865 <64709498+mu10-ar@users.noreply.github.com>
2021-11-01 13:58:10 +0500

    sddsa
    ssd
    
    Co-Authored-By: asohaib865 <92565082+asohaib865@users.noreply.github.com>

--- app/Controllers/Doctor.php
+++ app/Controllers/Doctor.php
@@ -138,9 +138,9 @@ class Doctor extends BaseController
                     'user_role' =>  $this->request->getPost('user_role')
 
                 ]);
-                $session->setFlashdata('success', 'Doctor Added, Data Saved Successfully');
                
                 $success = true;
+                 $session->setFlashdata('success', 'Doctor Added, Data Saved Successfully');
                 return  redirect()->to('doctorslist');
             } else {
                 $data['validation'] = $this->validator;
@@ -221,23 +221,16 @@ class Doctor extends BaseController
                     ]
                 ],
                 'email' => [
-                    'rules' => 'required|valid_email|is_unique[users.email]',
+                    'rules' => 'required|valid_email',
                     'errors' => [
                         'required' => 'please provide your email',
-                        'valid_email' => 'Please enter a valid Email',
-                        'is_unique' => 'This Email is Already Registered'
+                        'valid_email' => 'Please enter a valid Email'
+                      
 
                     ]
                 ],
-                'password' => [
-                    'rules' => 'required|min_length[10]',
-                    'errors' => [
-                        'required' => 'Create an Passwrod',
-                        'min_length' => 'Your Password is too Shot'
-
-
-                    ]
-                ],
+            
+               
                 'address' => [
                     'rules' => 'required|max_length[255]|alpha_numeric_space',
                     'errors' => [
@@ -260,12 +253,7 @@ class Doctor extends BaseController
                         'alpha_numeric_space' => "please insert career title"
                     ]
                 ],
-                'designation' => [
-                    'rules' => 'alpha_numeric_space',
-                    'errors' => [
-                        'alpha_numeric_space' => "please insert your designation"
-                    ]
-                ],
+               
                 'sex' => [
                     'rules' => 'required',
                     'errors' => [
@@ -291,119 +279,10 @@ class Doctor extends BaseController
                     ]
                 ],
 
-<<<<<<< HEAD
 
 
             ]);
 
-=======
-        if( $this->request->getMethod()=='post'){
-               $input=$this->validate([
-                'firstname'=> [
-                   'rules'=>'required|alpha_space',
-                   'errors'=>[ 
-                       'required'=>'name must be filled',
-                       'alpha_space'=>'name can contain only letters'
-                   ]],
-        'lastname'=> [
-                   'rules'=>'required|alpha_space',
-                   'errors'=>[ 
-                       'required'=>'last name must be filled',
-                       'alpha_space'=>'name can contain only letters'
-                 ]],
-        'email'=> [
-                   'rules'=>'required|valid_email',
-                   'errors'=>[ 
-                       'required'=>'please provide your email',
-                       'valid_email'=>'Please enter a valid Email'
-                       
-
-                   ]],
-       
-        'address'=> [
-                   'rules'=>'required|max_length[255]|alpha_numeric_space',
-                   'errors'=>[ 
-                       'required'=>'Please provide your address',
-                       'max_length'=>'Your Password is too Shot',
-                       'alpha_numeric_space'=>"Address Can't Contain Special Characters`"
-                      
-
-                   ]],
-        'specialist'=> [
-                   'rules'=>'alpha_numeric_space',
-                   'errors'=>[ 
-                       'alpha_numeric_space'=>"please insert your specialization"
-                   ]],
-        'career_title'=> [
-                   'rules'=>'alpha_numeric_space',
-                   'errors'=>[ 
-                       'alpha_numeric_space'=>"please insert career title"
-                   ]],
-        'designation'=> [
-                   'rules'=>'alpha_numeric_space',
-                   'errors'=>[ 
-                       'alpha_numeric_space'=>"please insert your designation"
-                   ]],
-        'sex'=> [
-                   'rules'=>'required',
-                   'errors'=>[ 
-                       'required'=>"Please select your gender"
-                   ]],
-         'department_name'=> [
-                   'rules'=>'required',
-                   'errors'=>[ 
-                       'required'=>"Please select your department"
-                   ]],
-        'birthday'=> [
-                   'rules'=>'required',
-                   'errors'=>[ 
-                       'required'=>"Please select your Date od Birth"
-                   ]],
-        'blood_group '=> [
-                   'rules'=>'required',
-                   'errors'=>[ 
-                       'required'=>"Please select your blood group"
-                   ]],
-
-                
-
-               ]);
-
-               
-           
-        
-             
-           if ($input==true) {
-               
-                    $model= new UserModel();
-                    $model->update($id,[
-                         'firstname'=> $this->request->getPost('firstname'),
-                'lastname' => $this->request->getPost('lastname'),
-                'email' => $this->request->getPost('email'),
-                'address' => $this->request->getPost('address'),
-                'specialist' => $this->request->getPost('specialist'),
-                'mobile_no' => $this->request->getPost('mobile_no'),
-                'sex' => $this->request->getPost('sex'),
-                'designation' => $this->request->getPost('designation'),
-                'department_name' => $this->request->getPost('department_name'),
-                'birthday' => $this->request->getPost('birthday'),
-                'blood_group' => $this->request->getPost('blood_group'),
-                'user_role'=>  $this->request->getPost('user_role')
-                    ]);
-                    
-                 
-                    
-                    
-               
-           }
-           else {
-                         $data['validation']=$this->validator;
-                    }
-        }
-        
-        $department=new DepartmentModel();
-        $data['department']=$department->getDepartmentRecord();
->>>>>>> 636859abac3349b7c3c13001abb3b9013a5f1fca
 
 
 
