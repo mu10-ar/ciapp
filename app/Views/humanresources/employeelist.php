8f6325f6db24abc8cbd91b96f6f74bf6e02eb2fe
asohaib865 <64709498+mu10-ar@users.noreply.github.com>
2021-11-02 16:16:20 +0500

    df
    df

--- app/Views/humanresources/employeelist.php
+++ app/Views/humanresources/employeelist.php
@@ -3,36 +3,35 @@
     <h2>Employees List </h2>
 </div>
 <div class="container">
-<div class="col-md-12">
-       <?php
-       $session=session();
-       if(!empty($session->getFlashdata('success'))){
-           ?>
-           <div class="alert alert-success">
-               <?php echo $session->getFlashdata('success') ?>
-           </div>
-           <?php
-       }
-       if(!empty($session->getFlashdata('error'))){
-           ?>
-           <div class="alert alert-danger">
-               <?php echo $session->getFlashdata('error') ?>
-           </div>
-           
-           <?php
-           
+    <div class="col-md-12">
+        <?php
+        $session = session();
+        if (!empty($session->getFlashdata('success'))) {
+        ?>
+            <div class="alert alert-success">
+                <?php echo $session->getFlashdata('success') ?>
+            </div>
+        <?php
+        }
+        if (!empty($session->getFlashdata('error'))) {
+        ?>
+            <div class="alert alert-danger">
+                <?php echo $session->getFlashdata('error') ?>
+            </div>
 
-       }   
-       ?>  
-        </div>  
+        <?php
 
-<?php
-$session=session();
-            if ($session->getFlashdata('success')) {
-                echo  $session->getFlashdata('success');
-               
-            }
-            ?>
+
+        }
+        ?>
+    </div>
+
+    <?php
+    // $session = session();
+    // if ($session->getFlashdata('success')) {
+    //     echo  $session->getFlashdata('success');
+    // }
+    ?>
     <table id="myTable" class="table table-striped">
         <thead>
             <tr>
@@ -47,39 +46,39 @@ $session=session();
         </thead>
         <tbody>
             <?php
-            
-            $id=1;
-            foreach($users as $user){?>
-            <tr>
 
-                <th scope="row"><?=$id?></th>
-                <td><?=$user['firstname']?></td>
-                <td><?=$user['lastname']?></td>
-                <td><?=$user['email']?></td>
-                <td><?=$user['address']?></td>
-                <td><?php
-                
-                if ($user['user_role']=="5") {
-                    echo"acountant";
-                }elseif ($user['user_role']=="6") {
-                    echo  'bed manager';
-                }elseif ($user['user_role']=="7") {
-                    echo "pharmacist";
-                }
-                elseif ($user['user_role']=="8") {
-                    echo "Lab technician";
-                }
-                
-                
-                
-                
-                ?></td>
-                <td> <a href="<?=base_url()?>/updateemployee/<?=$user['id']?>" class="btn btn-primary btn-sm">Edit</a>
-                    <a href="<?=base_url()?>/deleteemployee/<?=$user['id']?>" class="btn btn-danger btn-sm">Delete</a>
-                    <a href="<?=base_url()?>/profile/<?=$user['id']?>" class="btn btn-warning btn-sm">profile</a>
-                </td>
-            </tr>
-            <?php $id++;}?>
+            $id = 1;
+            foreach ($users as $user) { ?>
+                <tr>
+
+                    <th scope="row"><?= $id ?></th>
+                    <td><?= $user['firstname'] ?></td>
+                    <td><?= $user['lastname'] ?></td>
+                    <td><?= $user['email'] ?></td>
+                    <td><?= $user['address'] ?></td>
+                    <td><?php
+
+                        if ($user['user_role'] == "5") {
+                            echo "acountant";
+                        } elseif ($user['user_role'] == "6") {
+                            echo  'bed manager';
+                        } elseif ($user['user_role'] == "7") {
+                            echo "pharmacist";
+                        } elseif ($user['user_role'] == "8") {
+                            echo "Lab technician";
+                        }
+
+
+
+
+                        ?></td>
+                    <td> <a href="<?= base_url() ?>/updateemployee/<?= $user['id'] ?>" class="btn btn-primary btn-sm">Edit</a>
+                        <a href="<?= base_url() ?>/deleteemployee/<?= $user['id'] ?>" class="btn btn-danger btn-sm">Delete</a>
+                        <a href="<?= base_url() ?>/profile/<?= $user['id'] ?>" class="btn btn-warning btn-sm">profile</a>
+                    </td>
+                </tr>
+            <?php $id++;
+            } ?>
 
 
         </tbody>
